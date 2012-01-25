682286c16f9d83f023c01e0aa9e796e470bf59e9
Ben Lamb <ben@zurgy.org>
2012-01-25 13:24:52 +0000

    Initial Commit

---
+++ SampleTweet/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  SampleTweet
+//
+//  Created by Ben Lamb on 25/01/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ SampleTweet/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SampleTweet
+//
+//  Created by Ben Lamb on 25/01/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
